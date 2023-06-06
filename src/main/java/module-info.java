c31b000a54d926dbe9470d01576eeb59be5abb4c
Влад-Беляков <vladbelyakovv@gmail.com>
2023-06-06 21:21:29 +0300

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.project.chatfx {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.project.chatfx to javafx.fxml;
+    exports com.project.chatfx;
+}
